ffdd91acdc6875fc6d735508995c1c7f2693b174
Salah Alquraan <alquraansalah3gm@gmail.com>
2026-07-04 18:19:21 +0300

    Adding end to coverage
    Added a condition to end the program once the robot has covered the entire area

--- main.py
+++ main.py
@@ -1,50 +1,73 @@
 #!/usr/bin/env pybricks-micropython
-import sys
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
+
 ev3 = EV3Brick()
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
 line_sensor = ColorSensor(Port.S3)
-
 robot = DriveBase(left_motor, right_motor, wheel_diameter=60, axle_track=90)
-
 border_color = Color.RED
 
 def find_corner():
-    #this will later be follow the line to the corner
+    
     i = 0
     while i < 2:
         robot.drive(150, 0)
         if line_sensor.color() == border_color: 
             robot.stop()
-            ev3.speaker.beep(frequency=500, duration=1000)
             robot.turn(-90)
-            i+=1
+            i += 1
 
 def run_coverage():
-
     find_corner()
+    direction = -1 
+    task_finished = False
 
-    direction = -1
-    while True: 
+    while not task_finished: 
+       
         while line_sensor.color() != border_color:
             robot.drive(150, 0)
         
         robot.stop()
+        
+     
         robot.turn(90 * direction)
-        robot.straight(80)
-        robot.turn(90 * direction)
         
-        direction *= -1
+        distance_covered = 0
+        hit_border = False
         
-        # Find a way to end the program
-        # if total_distance > 5000: break
+        while distance_covered < 80:
+            robot.drive(100, 0)
+            if line_sensor.color() == border_color:
+                hit_border = True 
+                break
+            wait(10)
+            distance_covered += 1
+            
+        robot.stop()
         
+        
+        if hit_border:
+            
+            robot.turn(90 * direction) 
+            robot.drive(150, 0)        
+            wait(2000)                 
+            robot.stop()
+            task_finished = True     
+        else:
+            
+            robot.turn(90 * direction)
+            direction *= -1
+
+    
+    ev3.speaker.beep(frequency=1000, duration=2000)
+    robot.stop()
+
 try:
     run_coverage()
 except KeyboardInterrupt:
